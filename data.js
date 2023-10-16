1367c719e2e08c6b68966e893302d635fde2c77e
moustudiointerior <144911346+moustudiointerior@users.noreply.github.com>
2023-10-16 11:14:42 +0200

    Initial commit

---
+++ data.js
@@ -0,0 +1,79 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-2ganek-1",
+      "name": "2ganek 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.071824301125603,
+          "pitch": 0.5205380662354742,
+          "rotation": 0,
+          "target": "1-wejscie"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-wejscie",
+      "name": "wejscie",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.19388180879672845,
+          "pitch": 0.3018090309283359,
+          "rotation": 0,
+          "target": "0-2ganek-1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
